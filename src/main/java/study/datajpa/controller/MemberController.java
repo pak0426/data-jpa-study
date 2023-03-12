440ff8c416a4456ec97c4bf90fa6f117b6814127
hmmini <pak4184@naver.com>
2023-03-12 18:24:16 +0900

    Rest api에서 paging을 적용하는 법

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -28,8 +32,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        Page<Member> members = memberRepository.findAll(pageable);
+        return members.map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+    }
+
     @PostConstruct //초기화 작업을 수행하기 위해 메서드에 지정할 수 있습니다.
     public void init() {
-        memberRepository.save(new Member("memberA"));
+        for (int i=0; i< 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 }
