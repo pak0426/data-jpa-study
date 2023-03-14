3e188a43b49b451679034e86ca1013d60b06db4c
hmmini <pak4184@naver.com>
2023-03-14 19:40:07 +0900

    JPA 새로운 entity를 구별하는 방법

---
+++ src/test/java/study/datajpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,26 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.entity.Item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    public void save() {
+//        Item item = new Item();
+//        itemRepository.save(item);
+    }
+
+    @Test
+    public void save2() {
+        Item item = new Item("hello");
+        Item save = itemRepository.save(item);
+    }
+}
